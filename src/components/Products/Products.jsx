dd09c5f36e46091dca36610de0062db8fd3de617
saeedmohamedakkar <saeed.m6063@yahoo.com>
2024-03-23 16:03:03 +0200

    w/h

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, {  useState } from 'react';
 import { AiOutlineStar } from "react-icons/ai";
 
 const Products = ({ products,addCart }) => {
@@ -13,6 +13,7 @@ const Products = ({ products,addCart }) => {
 
 
     }
+
 
 
     return <>
